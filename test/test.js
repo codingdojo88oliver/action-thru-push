690a36db1ba84d66c43353d0b562d355c60ae146
Oliver Rosales <oliverjohnpr2013@gmail.com>
2021-02-26 12:14:07 +0800

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,20 @@
+process.env.NODE_ENV = 'test';
+
+const chai                  = require('chai');
+const expect                = chai.expect;
+
+class Fairness {
+    is_fair(a, b) {
+
+    }
+}
+
+describe("Fairness App", function() {
+    it("should return a success message if the two values are equal", function(){
+        let fairness = new Fairness();
+        let a = 1;
+        let b = 1;
+
+        expect(fairness.is_fair(a,b)).to.equal("Success!");
+    });
+});
